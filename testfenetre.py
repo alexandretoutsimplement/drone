71b8759414d1bae4dc9928eb8aab8cdf3eb094db
alexandretoutsimplement <115820918+alexandretoutsimplement@users.noreply.github.com>
2023-02-22 21:47:23 +0100

    Add files via upload

--- testfenetre.py
+++ testfenetre.py
@@ -1,6 +1,32 @@
 from tkinter import *
 import socket
 
+def getresp(label):
+	client_socket.timeout(1)
+
+	try: 
+		message, address=client_socket.recvfrom(1024)
+	except:
+		message= "Problème de connexion"
+	label.config(text=message)
+	
+def decoller():
+	message = "takeoff".encode()
+	client_socket.sendto(message, (SERVER_IP, SERVER_PORT))
+	getresp(label)
+def atterir():
+	message = "land".encode()
+	client_socket.sendto(message, (SERVER_IP, SERVER_PORT))
+	getresp(label)
+def droite():
+	message = "right 20".encode()
+	client_socket.sendto(message, (SERVER_IP, SERVER_PORT))
+	getresp(label)
+def gauche():
+	message = "left 20".encode()
+	client_socket.sendto(message, (SERVER_IP, SERVER_PORT))
+	getresp(label)
+	
 SERVER_IP = "192.168.10.1"
 SERVER_PORT =  8889
 
@@ -9,41 +35,48 @@ window=Tk()
 window.title("Manette")
 window.geometry("1920x1080")
 
+col1=100
+col2=200
+col3=300
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ligne1=100
+ligne2=200
+ligne3=300
 
-message = "Command".encode()
-client_socket.sendto(message, (SERVER_IP, SERVER_PORT))
+label= Label(window,text="")
+label.place(x=810, y=0)
 
-def on_button_1_click():
-        message = "takeoff".encode()
-client_socket.sendto(message, (SERVER_IP, SERVER_PORT))
 
-def on_button_2_click():
-        message = "land".encode()
-client_socket.sendto(message, (SERVER_IP, SERVER_PORT))
-    
-def on_button_3_click():
-        message = "right 20".encode()
-client_socket.sendto(message, (SERVER_IP, SERVER_PORT))
+button_1 = Button(window, text="Décoller", command=decoller)
+button_1.place(x=col2, y=ligne1)
 
-def on_button_4_click():
-        message = "left 20".encode()
-client_socket.sendto(message, (SERVER_IP, SERVER_PORT))
+button_2 = Button(window, text="Atterir", command=atterir)
+button_2.place(x=col2, y=ligne3)
 
-button_1 = Button(window, text="Monter", command=on_button_1_click)
-button_1.grid(row=0,column=1)
+button_3 = Button(window, text="Droite", command=droite)
+button_3.place(x=col3, y=ligne2)
 
-button_2 = Button(window, text="Décendre", command=on_button_1_click)
-button_2.grid(row=2, column=1)
+button_4 = Button(window, text="Gauche", command=gauche)
+button_4.place(x=col1, y=ligne2)
 
-button_3 = Button(window, text="Droite", command=on_button_1_click)
-button_3.grid(row=1, column=2)
+zonecam = Canvas(window, width="480", height="480", background= "black")
+zonecam.place(x=0, y=600)
 
-button_4 = Button(window, text="Gauche", command=on_button_1_click)
-button_3.grid(row=1, column=0)
+zonemap = Canvas(window, width="480", height="480", background= "blue")
+zonemap.place(x=1440, y=600)
 
 
 window.mainloop()
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+message = "command".encode()
+client_socket.sendto(message, (SERVER_IP, SERVER_PORT))
+
+
+getresp(label)
+
+
 
 client_socket.close()
